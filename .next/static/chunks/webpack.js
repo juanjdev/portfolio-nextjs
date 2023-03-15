6d988b3b5f588e8fa8c4f11e4178da434f28b265
Juan J. Blancop <juanjb.dev@gmail.com>
2023-03-15 01:53:25 -0500

    Added navigation bar and supporting dark theme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ab043321bf183b6"; }
+/******/ 		__webpack_require__.h = function() { return "101fc3a62831e24d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
